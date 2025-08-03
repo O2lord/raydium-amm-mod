51fa32a28a0fadedba9f7f4418fbecd8b8b4c3a6
O2lord <80071649+O2lord@users.noreply.github.com>
2025-08-03 15:13:21 +0100

    Updated error.rs

--- tradium/src/error.rs
+++ tradium/src/error.rs
@@ -4,31 +4,50 @@ use anchor_lang::prelude::*;
 pub enum TradiumError {
     #[msg("Unwhitelisted Transfer Hook Program")]
     UnwhitelistedTransferHookProgram,
-    
     #[msg("Invalid Token-2022 Mint")]
     InvalidToken2022Mint,
-    
     #[msg("Missing Transfer Hook Account")]
     MissingTransferHookAccount,
-    
     #[msg("Invalid Token Program")]
     InvalidTokenProgram,
-    
     #[msg("Insufficient Liquidity")]
     InsufficientLiquidity,
-    
     #[msg("Slippage Tolerance Exceeded")]
     SlippageToleranceExceeded,
-    
     #[msg("Invalid Pool State")]
     InvalidPoolState,
-    
     #[msg("Unauthorized")]
     Unauthorized,
-    
     #[msg("Math Overflow")]
     MathOverflow,
-    
     #[msg("Invalid Amount")]
     InvalidAmount,
+    #[msg("Invalid Deposit Amount")]
+    InvalidDepositAmount,
+    #[msg("Invalid Coin Token Program")]
+    InvalidCoinTokenProgram,
+    #[msg("Invalid PC Token Program")]
+    InvalidPcTokenProgram,
+    #[msg("Invalid Coin Vault")]
+    InvalidCoinVault,
+    #[msg("Invalid PC Vault")]
+    InvalidPcVault,
+    #[msg("Invalid LP Mint")]
+    InvalidLpMint,
+    #[msg("Insufficient Liquidity Minted")]
+    InsufficientLiquidityMinted,
+    #[msg("Insufficient Balance")]
+    InsufficientBalance,
+    #[msg("Empty Pool")]
+    EmptyPool,
+    #[msg("Insufficient Withdrawal")]
+    InsufficientWithdrawal,
+    #[msg("Missing Transfer Hook Program")]
+    MissingTransferHookProgram,
+    #[msg("Invalid Swap Direction")]
+    InvalidSwapDirection,
+    #[msg("Invalid Input Amount")]
+    InvalidInputAmount,
+    #[msg("Slippage Exceeded")]
+    SlippageExceeded,
 }
