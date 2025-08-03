562ffb21507bed2b393b0191f8aa9ca40bd65fd6
O2lord <80071649+O2lord@users.noreply.github.com>
2025-08-03 14:56:27 +0100

    Updated lib.rs

--- tradium/src/lib.rs
+++ tradium/src/lib.rs
@@ -26,4 +26,18 @@ pub mod tradium {
     ) -> Result<()> {
         instructions::initialize_pool(ctx, bump, initial_coin_amount, initial_pc_amount)
     }
+    pub fn deposit(ctx: Context<Deposit>, amount_coin: u64, amount_pc: u64) -> Result<()> {
+        instructions::deposit(ctx, amount_coin, amount_pc)
+    }
+    pub fn withdraw(ctx: Context<Withdraw>, lp_amount: u64) -> Result<()> {
+        instructions::withdraw(ctx, lp_amount)
+    }
+    pub fn swap(
+        ctx: Context<Swap>,
+        amount_in: u64,
+        min_amount_out: u64,
+        swap_direction: u8,
+    ) -> Result<()> {
+        instructions::swap(ctx, amount_in, min_amount_out, swap_direction)
+    }
 }
